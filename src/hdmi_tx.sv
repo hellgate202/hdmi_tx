c61e755246c9285dde1f06a7cc7c100bb44a86f7
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-04-20 17:31:32 +0300

    added wrapper and sync rst

--- src/hdmi_tx.sv
+++ src/hdmi_tx.sv
@@ -2,7 +2,7 @@ module hdmi_tx #(
   parameter int X_RES    = 1920,
   parameter int Y_RES    = 1080,
   parameter int PX_WIDTH = 10
-(
+)(
   input                px_clk_i,
   input                tmds_clk_i,
   input                rst_i,
@@ -27,37 +27,55 @@ logic [9 : 0] tmds_red;
 logic [9 : 0] tmds_green;
 logic [9 : 0] tmds_blue;
 
+logic rst_d1;
+logic rst_d2;
+logic px_clk_rst;
+
+always_ff @( posedge px_clk_i, rst_i )
+  if( rst_i )
+    begin
+      rst_d1 <= 1'b1;
+      rst_d2 <= 1'b1;
+    end
+  else
+    begin
+      rst_d1 <= 1'b0;
+      rst_d2 <= rst_d1;
+    end
+
+assign px_clk_rst = rst_d2;
+
 axi4_video_to_hv_conv #(
-  .X_RES        ( X_RES    ),
-  .Y_RES        ( Y_RES    ),
-  .PX_WIDTH     ( PX_WIDTH )
+  .X_RES        ( X_RES      ),
+  .Y_RES        ( Y_RES      ),
+  .PX_WIDTH     ( PX_WIDTH   )
 ) video_conv (
-  .clk_i        ( px_clk_i ),
-  .rst_i        ( rst_i    ),
-  .axi4_video_i ( video_i  ),
-  .red_o        ( red      ),
-  .green_o      ( green    ),
-  .blue_o       ( blue     ),
-  .px_valid_o   ( px_valid ),
-  .h_sync_o     ( h_sync   ),
-  .v_sync_o     ( v_sync   )
+  .clk_i        ( px_clk_i   ),
+  .rst_i        ( px_clk_rst ),
+  .axi4_video_i ( video_i    ),
+  .red_o        ( red        ),
+  .green_o      ( green      ),
+  .blue_o       ( blue       ),
+  .px_valid_o   ( px_valid   ),
+  .h_sync_o     ( h_sync     ),
+  .v_sync_o     ( v_sync     )
 );
 
 tmds_enc tmds_red_channel
 (
-  .clk_i         ( px_clk_i ),
-  .rst_i         ( rst_i    ),
-  .px_data_i     ( red      ),
-  .px_data_val_i ( px_valid ),
-  .ctl_0_i       ( 1'b0     ),
-  .ctl_1_i       ( 1'b0     ),
-  .tmds_data_o   ( tmds_red )
+  .clk_i         ( px_clk_i   ),
+  .rst_i         ( px_clk_rst ),
+  .px_data_i     ( red        ),
+  .px_data_val_i ( px_valid   ),
+  .ctl_0_i       ( 1'b0       ),
+  .ctl_1_i       ( 1'b0       ),
+  .tmds_data_o   ( tmds_red   )
 );
 
 tmds_enc tmds_green_channel
 (
   .clk_i         ( px_clk_i   ),
-  .rst_i         ( rst_i      ),
+  .rst_i         ( px_clk_rst ),
   .px_data_i     ( green      ),
   .px_data_val_i ( px_valid   ),
   .ctl_0_i       ( 1'b0       ),
@@ -67,20 +85,20 @@ tmds_enc tmds_green_channel
 
 tmds_enc tmds_blue_channel
 (
-  .clk_i         ( px_clk_i  ),
-  .rst_i         ( rst_i     ),
-  .px_data_i     ( blue      ),
-  .px_data_val_i ( px_valid  ),
-  .ctl_0_i       ( h_sync    ),
-  .ctl_1_i       ( v_sync    ),
-  .tmds_data_o   ( tmds_blue )
+  .clk_i         ( px_clk_i   ),
+  .rst_i         ( px_clk_rst ),
+  .px_data_i     ( blue       ),
+  .px_data_val_i ( px_valid   ),
+  .ctl_0_i       ( h_sync     ),
+  .ctl_1_i       ( v_sync     ),
+  .tmds_data_o   ( tmds_blue  )
 );
 
 hdmi_phy hdmi_red_channel
 (
   .px_clk_i       ( px_clk_i     ),
   .tmds_clk_i     ( tmds_clk_i   ),
-  .rst_i          ( rst_i        ),
+  .rst_i          ( px_clk_rst   ),
   .tmds_red_i     ( tmds_red     ),
   .tmds_green_i   ( tmds_green   ),
   .tmds_blue_i    ( tmds_blue    ),
---
+++ src/hdmi_tx_wrap.sv
@@ -0,0 +1,59 @@
+module hdmi_tx_wrap #(
+  parameter int X_RES    = 1920,
+  parameter int Y_RES    = 1080,
+  parameter int PX_WIDTH = 10
+)(
+  input          px_clk_i,
+  input          tmds_clk_i,
+  input          rst_i,
+  input [31 : 0] video_i_tdata,
+  input          video_i_tvalid,
+  output         video_i_treaady,
+  input          video_i_tuser,
+  input          video_i_tlast,
+  output         hdmi_tx2_p_o,
+  output         hdmi_tx2_n_o,
+  output         hdmi_tx1_p_o,
+  output         hdmi_tx1_n_o,
+  output         hdmi_tx0_p_o,
+  output         hdmi_tx0_n_o,
+  output         hdmi_clk_p_o,
+  output         hdmi_clk_n_o
+);
+
+axi4_lite_if #(
+  .DATA_WIDTH  ( 32       ),
+  .TID_WIDTH   ( 1        ),
+  .TDEST_WIDTH ( 1        ),
+  .TUSER_WIDTH ( 1        )
+) video_i (
+  .clk_i       ( px_clk_i ),
+  .aresetn     ( !rst_i   )
+);
+
+assign video_i.tdata  = video_i_tdata;
+assign video_i.tvalid = video_i_tvalid;
+assign video_i_tready = video_i.tready;
+assign video_i.tlast  = video_i_tlast;
+assign video_i.tuser  = video_i_tuser;
+
+hdmi_tx #(
+  .X_RES        ( X_RES        ),
+  .Y_RES        ( Y_RES        ),
+  .PX_WIDTH     ( PX_WIDTH     )
+) hdmi_tx (
+  .px_clk_i     ( px_clk_i     ),
+  .tmds_clk_i   ( tmds_clk_i   ),
+  .rst_i        ( rst_i        ),
+  .video_i      ( video_i      ),
+  .hdmi_tx2_p_o ( hdmi_tx2_p_o ),
+  .hdmi_tx2_n_o ( hdmi_tx2_n_o ),
+  .hdmi_tx1_p_o ( hdmi_tx1_p_o ),
+  .hdmi_tx1_n_o ( hdmi_tx1_n_o ),
+  .hdmi_tx0_p_o ( hdmi_tx0_p_o ),
+  .hdmi_tx0_n_o ( hdmi_tx0_n_o ),
+  .hdmi_clk_p_o ( hdmi_clk_p_o ),
+  .hdmi_clk_n_o ( hdmi_clk_n_o )
+);
+
+endmodule
